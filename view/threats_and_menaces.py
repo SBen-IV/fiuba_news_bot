9d84f381ad713f7ec65db52a77efc1d15cb1738c
SBen-IV <sinneo@fi.uba.ar>
2023-03-24 15:10:09 -0300

    feat: emoji

--- view/threats_and_menaces.py
+++ view/threats_and_menaces.py
@@ -1,9 +1,12 @@
 from view.imprenta import Imprenta
 from telegram import ParseMode
+from emoji import emojize
+
+MAS_INFORMACION = emojize(":information: Más información")
 
 class ThreatsAndMenaces(Imprenta):
     def enviar_noticias(self, chat, noticias) -> None:
         for noticia in noticias:    
             chat.send_message("<b>" + noticia.titulo + "</b>" + "\n\n" + noticia.descripcion + "\n\n" + \
-                  "<a href= \"" + noticia.url + "\">" + "MAS_INFORMACION" + "</a>\n",
+                  "<a href= \"" + noticia.url + "\">" + MAS_INFORMACION + "</a>\n",
                   parse_mode=ParseMode.HTML)
